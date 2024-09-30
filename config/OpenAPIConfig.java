8b138682840a38b3a587d33b367d5470dc8d0ea0
Yvonne Kwarteng <yvonnekcnwl@yahoo.co.uk>
2024-09-30 21:59:57 +0100

    swagger configuration

---
+++ config/OpenAPIConfig.java
@@ -0,0 +1,19 @@
+package com.auction.product_service.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+    @Bean
+    public OpenAPI productServiceAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Product Service API")
+                        .description("This is the Rest API for the Product Service")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0")));
+    }
+}
